8759c6f247ed4895fd298be5ade9ca4f48f78837
MatheusAraruna <matheusararuna90@gmail.com>
2023-10-07 04:00:13 -0300

    Add tests to project

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 const buildEslintCommand = (filenames) => [
   `prettier --write ${filenames.join(' ')}`,
-  `npm run lint --fix . ${filenames.join(' --file')}`
+  `npm run lint --fix . ${filenames.join(' --file')}`,
+  `npm test -- --findRelatedTests ${filenames.join(' ')}`
 ]
 
 module.exports = {
